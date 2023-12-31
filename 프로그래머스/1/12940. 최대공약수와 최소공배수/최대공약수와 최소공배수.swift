c1bd22644ad42360204570aa92c48219d9c4975a
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2023-12-31 10:56:39 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,11 @@
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
+
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    return [gcd(n, m), n * m / gcd(n, m)]
+}
