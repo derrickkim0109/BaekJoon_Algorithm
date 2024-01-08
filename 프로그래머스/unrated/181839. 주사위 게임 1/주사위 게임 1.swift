f4a12865bba82de2c7d0b0cc0c379f621482f983
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-01-08 12:43:51 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    if a % 2 != 0 && b % 2 != 0 {
+        return (a * a) + (b * b)
+    } else if a % 2 != 0 || b % 2 != 0 {
+        return 2 * (a + b)
+    } else if a % 2 == 0 && b % 2 == 0 {
+        return abs(a - b)
+    }
+    
+    return 0
+}
