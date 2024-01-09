d979daf4c56f1a5ac9c26913f3a29b6adbec4732
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-01-09 13:00:42 +0900

    [level unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ board:[[Int]], _ k:Int) -> Int {
+    var answer = 0
+    
+    for i in 0..<board.count {
+        for j in 0..<board[0].count {
+            if i + j <= k {
+                answer += board[i][j]
+            }
+        }
+    }
+    
+    return answer
+}
