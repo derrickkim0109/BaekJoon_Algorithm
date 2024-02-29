a5411c0908a80465efd3bc0e842f36b8cb5b80af
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-02-29 16:52:40 +0900

    [Gold V] Title: 이진 검색 트리, Time: 56 ms, Memory: 69744 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.swift
@@ -0,0 +1,32 @@
+func findHigherIndex(start: Int, end: Int, compare: Int) -> Int {
+    var higherIndex: Int = end
+
+    for i in start ..< end {
+        if treeList[i] > compare {
+            higherIndex = i
+            break
+        }
+    }
+    
+    return higherIndex
+}
+
+func postOrder(start: Int, end: Int) {
+    if start >= end {
+        return
+    }
+    
+    let higherIndex: Int = findHigherIndex(start: start + 1, end: end, compare: treeList[start])
+    
+    postOrder(start: start + 1, end: higherIndex)
+    postOrder(start: higherIndex, end: end)
+    print(treeList[start])
+}
+
+var treeList: [Int] = []
+
+while let input = readLine(), let n = Int(input) {
+    treeList.append(n)
+}
+
+postOrder(start: 0, end: treeList.count)
