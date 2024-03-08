cc6a18e5a37c80876b8cdb1adaf9c804f7a525ef
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-03-08 10:05:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -1,15 +1,42 @@
 let n = Int(readLine()!)!
-var array = readLine()!.split(separator: " ").map { Int(String($0))! }
-var dp = Array(repeating: 0, count: n + 1)
+var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var lis = Array(repeating: 0, count: 1001)
+var j = 0
+var i = 1
 
-for i in 0..<n {
-    dp[i + 1] = 1
+lis[0] = arr[0] // lis 배열의 맨 첫번째 값은 arr[0]으로 초기화
 
-    for j in 0..<i+1 {
-        if array[i] > array[j] {
-            dp[i + 1] = max(dp[i + 1], dp[j + 1] + 1)
-        }
+// arr의 두번째부터 마지막까지 하나씩 lis와 비교하면서 넣어준다.
+while i < n {
+    // lis 배열의 맨 뒤의 값보다 arr[i]가 더 크면 그것을 lis 배열 맨 뒤에 넣어준다.
+    if lis[j] < arr[i] {
+        lis[j + 1] = arr[i]
+        j += 1
     }
+    // arr[i]값이 더 작으면, arr[i]의 값이 lis 배열 중 어느 곳에 들어올지 이분탐색한다.
+    else {
+        let idx = binarySearch(0, j, arr[i]) // 0부터 j까지 탐색하면서 arr[i]가 들어갈 수 있는 위치를 찾아서 idx에 반환
+        lis[idx] = arr[i]
+    }
+    i += 1
 }
 
-print(dp.max()!)
+print(j + 1)
+
+// LIS를 유지하기 위해 숫자가 들어갈 위치를 이분탐색으로 찾는 함수
+func binarySearch(_ left: Int, _ right: Int, _ target: Int) -> Int {
+    var left = left
+    var right = right
+    var mid: Int
+
+    while left < right {
+        mid = (left + right) / 2 // 중간값 설정
+
+        if lis[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return right
+}
