1a6eef17b2450b68360ec169b9d3c19a18bfedc5
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-03-10 14:47:27 +0900

    [Gold IV] Title: 거짓말, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.swift
+++ 백준/Gold/1043. 거짓말/거짓말.swift
@@ -1,15 +1,19 @@
 let nm = readLine()!.split(separator: " ").map { Int($0)! }
 let n = nm[0], m = nm[1]
-var array = readLine()!.split(separator: " ").map { Int($0)! }
+var trueManList = readLine()!.split(separator: " ").map { Int($0)! }
 var party = [[Int]]()
 var graph = [[Int]](repeating: [], count: n+1)
 var visited = [Bool](repeating: false, count: n+1)
 
 for _ in 0..<m {
     let data = readLine()!.split(separator: " ").map { Int($0)! }
-    party.append(Array(data[1...]))
-    for i in 0..<data[0] {
-        for j in i+1..<data[0] {
+    let totalPeopleCount = data[0]
+    let peopleSequence = Array(data[1...])
+
+    party.append(peopleSequence)
+
+    for i in 0..<totalPeopleCount {
+        for j in i+1..<totalPeopleCount {
             let u = data[i+1], v = data[j+1]
             graph[u].append(v)
             graph[v].append(u)
@@ -25,8 +29,8 @@ func dfs(_ x: Int) {
 }
 
 // 진실을 알고 있는 사람들로부터 탐색 시작
-for i in 1..<array.count {
-    dfs(array[i])
+for i in 1..<trueManList.count {
+    dfs(trueManList[i])
 }
 
 // 진실을 모르는 사람만 있는 파티의 수 세기
