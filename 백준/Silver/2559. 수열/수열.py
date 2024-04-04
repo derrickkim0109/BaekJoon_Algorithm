4ae160e0ee04f03151943429e5dd7ce68e359871
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-04-05 08:27:06 +0900

    [Silver III] Title: 수열, Time: 140 ms, Memory: 124772 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,13 @@
+import sys 
+
+input = sys.stdin.readline
+
+n, k = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+result = [sum(nums[:k])]
+
+for i in range(n - k):
+    result.append(result[i] - nums[i] + nums[i + k])
+
+print(max(result))
