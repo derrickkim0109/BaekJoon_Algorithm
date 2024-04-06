4548d44f4f6398ca2b7533a81967b7e503eb0e8f
Derrick Kim <59466342+derrickkim0109@users.noreply.github.com>
2024-04-06 12:06:23 +0900

    [Gold V] Title: 두 용액, Time: 144 ms, Memory: 126372 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+
+start, end = 0, n - 1
+
+minValue = float('inf')
+answer = []
+
+while start < end:
+    value = nums[start] + nums[end]
+
+    if abs(value) < minValue:
+        minValue = abs(value)
+        answer = [nums[start], nums[end]]
+    
+    if value < 0:
+        start += 1
+    elif value > 0:
+        end -= 1
+    else: 
+        break
+
+print(*answer)
